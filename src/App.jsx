c392019b346e9dc562da35f72dcab6cf9330e5c6
Joel Kasisi <joelkasisi10@gmail.com>
2024-11-20 18:21:17 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TaskManagementDashboard from "./components/TaskManagementDashboard";
+
+function App() {
+  return (
+    <>
+      <TaskManagementDashboard />
+    </>
+  );
+}
+
+export default App;
